6336d149e34da029a0d2e91b19e506338b903a98
Naofumi Hattori <hattorix0@gmail.com>
2021-08-04 21:24:29 +0900

    Mac の場合に環境変数 LANG を設定する

--- 90_mac.sh
+++ 90_mac.sh
@@ -4,5 +4,7 @@ if [[ "$(uname)" != 'Darwin' ]]; then
     return
 fi
 
+export LANG=ja_JP.UTF-8
+
 # completion があれば読み込む
 [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
